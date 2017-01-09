09964fbd5b4188fb200fd72233ccb582952019fb
truongle <htr.lee@gmail.com>
2017-01-09 11:01:04 +0000

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-
+    validates: :username, 
+                presence:true, 
+                uniqueness: { case_sensitive:false }, 
+                length:{minimum:6,maximum:30}
+    VALID_EMAIL_REGEX= /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates: :email, 
+                presence:true,           
+                uniqueness: { case_sensitive:false }, 
+                length: { minimum:6, maximum:30 } ,
+                format: { with: VALID_EMAIL_REGEX }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109102720) do
+ActiveRecord::Schema.define(version: 20170109104100) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170109102720) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "password"
+    t.text     "email"
   end
 
 end
